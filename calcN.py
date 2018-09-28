b8a988a851a2e18404ac470c696d47409d160cc6
Sui Guan <sui.kun.guan@gmail.com>
2018-09-27 20:34:53 -0700

    Added sample size alpha beta calc if population std is known

---
+++ calcN.py
@@ -0,0 +1,15 @@
+from scipy import stats
+import math
+
+useT = False
+
+alpha = 0.05
+beta = 0.1
+ua = 3500
+ub = 3800 #high side
+std = 430
+
+za = stats.norm.ppf(1-(alpha/2))
+zb = stats.norm.ppf(1-beta)
+n = ((za+zb)*std/(ub-ua))**2
+print("useT %d, n = %s" % (useT,n))
