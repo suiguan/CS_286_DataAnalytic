aa904b31c441e5eb07346a51abd6e309fab66f02
Sui Guan <sui.kun.guan@gmail.com>
2018-09-30 15:32:39 -0700

    Added CI and Hypothesis test python program

---
+++ ci.py
@@ -0,0 +1,25 @@
+from scipy import stats
+import numpy as np
+import math
+
+#data = np.array([107,119,99,116,101,121,114,120,104,88,114,124,152,100,125,114,95,117])
+
+useT = True #set to True if population std is unknown
+n = 58 #data.shape[0]
+xBar = 25.0 #np.mean(data) 
+std = 2.7 #np.std(data)
+CITwoSideConfident = 0.95
+
+df = n - 1
+print("%s 2-side CI with n=%s, xBar=%s, std=%s, df=%s" % (CITwoSideConfident, n, xBar, std, df))
+probRight = (1 - CITwoSideConfident) / 2
+if useT: 
+   t = stats.t.ppf(1-probRight, df+1)
+   CI_0 = (-t * std / math.sqrt(n)) + xBar
+   CI_1 = (t * std / math.sqrt(n)) + xBar
+else: 
+   z = stats.norm.ppf(1-probRight)
+   CI_0 = (-z * std / math.sqrt(n)) + xBar
+   CI_1 = (z * std / math.sqrt(n)) + xBar
+print("%s: %s%% CI = (%s , %s)" % ("t-dis" if useT else "z-dis", CITwoSideConfident*100, CI_0, CI_1))
+
---
+++ htest.py
@@ -0,0 +1,31 @@
+from scipy import stats
+import math
+
+useT = True #set it to True if population std is unknown
+n = 86
+xBar = 87 
+std = 16.0 
+hytestAlpha = 0.10
+testRelation = "=" 
+populationMeanTest = 84 
+
+df = n - 1
+print("n = %s, xBar = %s, std = %s, populationMeanTest = %s, df = %s" %\
+      (n, xBar, std, populationMeanTest, df))
+print("testing H0: u %s %s, alpha = %s" % (testRelation, populationMeanTest, hytestAlpha))
+
+cValue = (xBar - populationMeanTest) / (std / math.sqrt(n))
+if useT: probLeft = stats.t.cdf(cValue, df+1)
+else: probLeft = stats.norm.cdf(cValue)
+print("%s = %s , lookup prob to the left of this is %s (right %s)" %\
+   ("t-dis" if useT else "z-dis", cValue, probLeft, 1 - probLeft))
+
+if testRelation == "=": 
+   if probLeft >= 0.5: pVal = 2 * (1-probLeft)
+   else: pVal = 2 * probLeft
+elif testRelation == ">=": pVal = probLeft
+elif testRelation == "<=": pVal = 1-probLeft
+else: raise Exception("Invalid test relation %s" % testRelation)
+
+print("p-value = %s, %s" % (pVal, "NOT Reject" if pVal >= hytestAlpha else "Reject"))
+
